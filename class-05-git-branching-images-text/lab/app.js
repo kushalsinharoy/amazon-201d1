4f5e29b5be4cd794bd38afef424b72919cf254e0
Kushal Sinha Roy <kushalr@amazon.com>
2019-02-25 00:10:29 -0800

    Problem 4 sumArray() completed.

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -93,12 +93,24 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    var sumOfElementsInArray = 0;
+    var secondElement = '';
+    var secondElementPartTwo = ' was passed in as an array of numbers, and ';
+    var secondElementPartThree = ' is their sum.';
+    for(var i = 0; i<sumArr.length; i++) {
+        sumOfElementsInArray = sumOfElementsInArray + sumArr[i];
+        secondElement = secondElement + sumArr[i] + ',';
+    }
+    // trim the last charater and append the other parts.
+    secondElement = secondElement.slice(0, -1);
+    secondElement =  secondElement + secondElementPartTwo + sumOfElementsInArray + secondElementPartThree;
 
+    return[sumOfElementsInArray, secondElement];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
